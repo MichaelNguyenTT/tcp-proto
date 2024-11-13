73ef006c6ae92a9317ac0a7c84809905d8c25952
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2024-11-13 01:03:46 -0800

    packets are confusing...

--- internal/packets/packets.go
+++ internal/packets/packets.go
@@ -1,6 +1,10 @@
 package packets
 
-import "io"
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
 
 const (
 	HEADER_SIZE     = 3
@@ -14,29 +18,74 @@ const (
 	PacketError
 )
 
-type Encoding uint8
-
-const (
-	EncodeBytes Encoding = iota
-	EncodeString
-)
-
 type Packet struct {
-	data []byte
-	len  int
+	data   []byte
+	pktLen int
 }
 
-// encoder to write the packet
 type PacketEncoder interface {
 	io.Reader
 	Type() uint8
-	Encoder() Encoding
+	Encoder() ([]byte, error)
 }
 
-// packet protocol structure
-// [type: 1 byte][packet size: 2 bytes][data: number of bytes]
+var (
+	ErrPktExceededMax = fmt.Errorf("buffer exceeded maximum packet size: %d", MAX_PACKET_SIZE)
+	ErrPktTooSmall    = fmt.Errorf("buffer data too small for packet")
 
-// packet type = uint8 1 byte for memory efficiency
-// packet size = length of the data stored with byte order as uint16, taking 2 bytes
+	ErrInvalidHeader = fmt.Errorf("invalid packet header")
+	ErrInvalidType   = fmt.Errorf("invalid packet type")
+)
+
+// packet protocol structure
+// [packet size: 2 bytes][type: 1 bytes][data: n of bytes]
 
 //TODO: packet logic
+
+func NewPacket(pkt PacketEncoder) Packet {
+	buf := make([]byte, MAX_PACKET_SIZE)
+
+	data := buf[HEADER_SIZE:]
+	n, _ := pkt.Read(data)
+
+	binary.BigEndian.PutUint16(buf[0:2], uint16(n))
+	buf[2] = byte(PacketType(PacketMessage))
+
+	return Packet{
+		data:   buf,             // data is the entire buffered packet
+		pktLen: HEADER_SIZE + n, // pktLen => is basically length of byte slice of the payload + header size protocol || 4 + N
+	}
+}
+
+// allocate empty buffer size with HEADER + data
+func allocateBuffer(data []byte) []byte {
+	pktSize := make([]byte, HEADER_SIZE+len(data))
+	return pktSize
+}
+
+// HACK: feels wonky....
+func (p *Packet) Type() PacketType {
+	return PacketType(p.data[2])
+}
+
+// encoder will transform byte sequence to transmittable bytes
+func (p *Packet) Encoder() ([]byte, error) {
+	pktBuf := allocateBuffer(p.data)
+
+	binary.BigEndian.PutUint16(pktBuf[0:2], uint16(p.pktLen))
+	pktBuf[2] = byte(p.Type())
+
+	copy(pktBuf[HEADER_SIZE:], p.data)
+
+	return pktBuf, nil
+}
+
+func (p *Packet) Read(data []byte) (int, error) {
+	if len(data) < p.pktLen {
+		return 0, ErrPktTooSmall
+	}
+
+	copy(data, p.data[:p.pktLen])
+
+	return p.pktLen, nil
+}
