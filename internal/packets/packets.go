701a828aecf97cadcbd3225d8df6173cb8b4d07f
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2024-11-14 23:44:31 -0800

    packet encoder

--- internal/packets/packets.go
+++ internal/packets/packets.go
@@ -19,13 +19,13 @@ const (
 )
 
 type Packet struct {
-	data   []byte
-	pktLen int
+	pkt []byte
+	len int
 }
 
 type PacketEncoder interface {
 	io.Reader
-	Type() uint8
+	PacketType() uint8
 	Encoder() ([]byte, error)
 }
 
@@ -40,52 +40,67 @@ var (
 // packet protocol structure
 // [packet size: 2 bytes][type: 1 bytes][data: n of bytes]
 
-//TODO: packet logic
-
-func NewPacket(pkt PacketEncoder) Packet {
+// new packet tester
+func NewPacket(pkt PacketEncoder) *Packet {
 	buf := make([]byte, MAX_PACKET_SIZE)
-
 	data := buf[HEADER_SIZE:]
 	n, _ := pkt.Read(data)
 
-	binary.BigEndian.PutUint16(buf[0:2], uint16(n))
-	buf[2] = byte(PacketType(PacketMessage))
-
-	return Packet{
-		data:   buf,             // data is the entire buffered packet
-		pktLen: HEADER_SIZE + n, // pktLen => is basically length of byte slice of the payload + header size protocol || 4 + N
+	return &Packet{
+		pkt: buf,
+		len: HEADER_SIZE + n,
 	}
 }
 
-// allocate empty buffer size with HEADER + data
-func allocateBuffer(data []byte) []byte {
-	pktSize := make([]byte, HEADER_SIZE+len(data))
-	return pktSize
-}
-
-// HACK: feels wonky....
-func (p *Packet) Type() PacketType {
-	return PacketType(p.data[2])
-}
-
-// encoder will transform byte sequence to transmittable bytes
-func (p *Packet) Encoder() ([]byte, error) {
-	pktBuf := allocateBuffer(p.data)
-
-	binary.BigEndian.PutUint16(pktBuf[0:2], uint16(p.pktLen))
-	pktBuf[2] = byte(p.Type())
-
-	copy(pktBuf[HEADER_SIZE:], p.data)
-
-	return pktBuf, nil
+func (p *Packet) PacketType() PacketType {
+	return PacketType(p.pkt[2])
 }
 
 func (p *Packet) Read(data []byte) (int, error) {
-	if len(data) < p.pktLen {
+	if len(data) < p.len {
 		return 0, ErrPktTooSmall
 	}
 
-	copy(data, p.data[:p.pktLen])
+	copy(data, p.pkt[:p.len])
 
-	return p.pktLen, nil
+	return p.len, nil
+}
+
+func (p *Packet) createPacketBuffer() []byte {
+	buf := make([]byte, p.len)
+	return buf
+}
+
+func (p *Packet) getPacketDataLength() int {
+	return p.len - HEADER_SIZE
+}
+
+func (p *Packet) Encoder() ([]byte, error) {
+	pkt := p.createPacketBuffer()
+
+	if len(p.pkt) < len(pkt) {
+		return nil, ErrPktExceededMax
+	}
+
+	dataLeng := p.getPacketDataLength()
+
+	binary.BigEndian.PutUint16(pkt[0:2], uint16(dataLeng))
+	pkt[2] = byte(p.PacketType())
+
+	copy(pkt[HEADER_SIZE:], p.pkt[HEADER_SIZE:p.len])
+
+	return pkt, nil
+}
+
+func (p *Packet) WritePacket(w io.Writer) (int, error) {
+	if p.len > MAX_PACKET_SIZE {
+		return 0, ErrPktExceededMax
+	}
+
+	n, err := w.Write(p.pkt[:p.len])
+	if err != nil {
+		return 0, nil
+	}
+
+	return n, nil
 }
---
+++ internal/packets/packets_test.go
@@ -0,0 +1,91 @@
+package packets_test
+
+import (
+	"bytes"
+	"tcpserver/internal/packets"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	HEADER_SIZE     = 3
+	MAX_PACKET_SIZE = 1024
+)
+
+type TestPacket struct {
+	pkt []byte
+	len int
+}
+
+type PacketType uint8
+
+const (
+	PacketMessage PacketType = iota
+)
+
+var testData = []byte("hello world")
+
+func (p *TestPacket) Read(data []byte) (int, error) {
+	return copy(data, testData), nil
+}
+
+func (p *TestPacket) Encoder() ([]byte, error) {
+	return p.pkt, nil
+}
+
+func (p *TestPacket) PacketType() uint8 {
+	return uint8(PacketMessage)
+}
+
+func TestNewPacket(t *testing.T) {
+	testpkt := &TestPacket{}
+
+	pkt := packets.NewPacket(testpkt)
+
+	buff := make([]byte, 0, 50)
+	b := bytes.NewBuffer(buff)
+
+	n, err := pkt.WritePacket(b)
+
+	require.NoError(t, err, "packet logic had an error")
+	require.Equal(t, n, HEADER_SIZE+len(testData), "n packet length should equal to testData")
+	require.Equal(t, testData, buff[HEADER_SIZE:n], "data bytes should be equal to testData")
+}
+
+type testMessage struct {
+	content []byte
+	pType   PacketType
+}
+
+func (m *testMessage) Read(data []byte) (int, error) {
+	return copy(data, m.content), nil
+}
+
+func (m *testMessage) PacketType() uint8 {
+	return uint8(m.pType)
+}
+
+func (m *testMessage) Encoder() ([]byte, error) {
+	return m.content, nil
+}
+
+func TestPacketEncoderFormat(t *testing.T) {
+	payload := []byte{0x01, 0x02, 0x03}
+	msg := &testMessage{
+		content: payload,
+		pType:   PacketMessage,
+	}
+
+	p := packets.NewPacket(msg)
+	encoded, err := p.Encoder()
+
+	expected := []byte{
+		0x00, 0x03, // length (3)
+		0x00,             // type (PacketMessage)
+		0x01, 0x02, 0x03, // payload
+	}
+
+	require.NoError(t, err, "Encoder() error: %v")
+	require.Equal(t, expected, encoded, "should have same encoding format as expected")
+}
