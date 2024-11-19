a148620b87d1be147524e00f2400a4680dfcac3a
MichaelNguyenTT <michaelnguyenn94@gmail.com>
2024-11-18 16:09:36 -0800

    moved helper funcs

--- internal/packets/packets.go
+++ internal/packets/packets.go
@@ -153,28 +153,3 @@ func PacketFromReader(reader io.Reader) (*Packet, error) {
 	pkt := createPacketFromRawBytes(pktBuf)
 	return &pkt, nil
 }
-
-// get the header packet length
-func extractPacketLength(data []byte) uint16 {
-	pktLength := binary.BigEndian.Uint16(data[0:2])
-	return pktLength
-}
-
-func createPacketFromRawBytes(data []byte) Packet {
-	// need some sort of assertions for simulation testing
-	// length := len(data) - HEADER_SIZE
-	// pktLen := extractPacketLength(data)
-
-	return Packet{
-		pkt: data,
-		len: len(data),
-	}
-}
-
-func copyPacketData[T any](to, from []T) int {
-	return copy(to, from)
-}
-
-func removePacketData[T any](to, from []T) int {
-	return copy(to, from)
-}
